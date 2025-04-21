a529d53cd71b092a7512af37ae5a92da56366764
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-21 22:18:07 +0530

    Add

---
+++ Cohort3.0/Week-1.1/index.js
@@ -0,0 +1,50 @@
+// let firstName = "mohit"; 
+// const age = 30; 
+// var isStudent = true;  
+
+
+// console.log(firstName); 
+// console.log(age); 
+// console.log(isStudent); 
+
+
+// console.log("hello")
+// ----- 
+
+// var isBoy = false; 
+// console.log(isBoy);  
+// isBoy = "mohit";   
+// isBoy = 10; 
+// console.log(isBoy); 
+
+
+// let Pname = "john"; 
+// const age = 30; 
+// var isStudent = true;  
+
+// console.log(Pname); 
+// console.log(age); 
+// console.log(isStudent);  
+
+// ------ Data types ----------- 
+
+// let num = 23; 
+// let string = "hey mohit"; 
+// let isActive = true; 
+
+// console.log(num); 
+
+// let users = ["mohit", "babuaan","ara k"]; 
+// console.log(); 
+ 
+let age = 4; 
+let canVote = (age > 18); 
+console.log(canVote); 
+
+
+
+
+
+
+
+
