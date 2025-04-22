c7426fe2fc30b55e77fce578825081abe9021c9a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-04-22 11:48:46 +0530

    Add

--- Cohort3.0/Week-1.1/index.js
+++ Cohort3.0/Week-1.1/index.js
@@ -37,9 +37,176 @@
 // let users = ["mohit", "babuaan","ara k"]; 
 // console.log(); 
  
-let age = 4; 
-let canVote = (age > 18); 
-console.log(canVote); 
+// condition ------------- 
+
+// let age = 4; 
+// let canVote = (age > 18); 
+// console.log(canVote); 
+ 
+// operators :- 
+
+// let sum = 10 + 5; 
+// console.log(sum);   
+
+// let equal = (10 == 10); 
+// console.log(equal);  
+
+// let isTrue = (true && false); 
+// console.log(isTrue); 
+ 
+
+
+// function :- 
+
+// function greet(name) {
+//   return "Hello," + name; 
+// } 
+
+// let ans = greet("mohit"); 
+// let ans2 = greet("rohit"); 
+// let ans3 = greet("koshal"); 
+
+// console.log(ans); 
+// console.log(ans2); 
+// console.log(ans3); 
+  
+
+// loop 
+
+// let users = ["mohit", "avinash", "koshal", "rinchu","gautam"]; 
+// for(let i=0; i<4; i++){
+//   console.log(users[i]); 
+// } 
+ 
+// * 
+// let students = ["raju","rohit","ranjan"]; 
+// for(let i=0; i<3; i++){
+//   console.log(students[i]); 
+// }
+
+// *
+//  let user1Name = "harkirat"; 
+//  let user1age = 90; 
+//  let user1gender = "male"; 
+
+//  let user1 =  {
+//   name: "raju bhai", 
+//   age: 20, 
+//   gender: "male"
+//  }
+ 
+//  console.log(user1.name)
+//  console.log(user1["name"])
+
+ 
+// * 
+// function greet(user) {
+//   console.log("hii " + user.name + " your age is " + user.age); 
+
+// }
+
+// let user = { 
+//   name: "saurabh", 
+//   age: 18
+// }
+ 
+// greet(user); 
+
+// ---
+
+function greet(user) {
+  console.log("my " + user.name + " name is " + user.name2); 
+} 
+
+let user = {
+  name: "college", 
+  name2: "dav"
+
+} 
+greet(user); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------
+
+// Assignment questions :- 
+
+// function sum(a, b) {
+//   let totalsum = a + b; 
+//   return totalsum; 
+
+// } 
+
+// let ans = sum(12, 10); 
+// let ans1 = sum(90, 23); 
+
+// console.log(ans); 
+// console.log(ans1); 
+
+// ------------------------- 
+//  Qs 1 - 
+
+// function sum(a, b){ 
+//   let result = a + b; 
+//   return result; 
+  
+// }
+// let ans = sum("5",+ 4); 
+// console.log(ans);  
+
+// Qs 2 :- 
+
+// let age = 20; 
+// let canVote = age > 18 ; 
+// console.log("you can vote = " + canVote); 
+ 
+// or 
+
+// let age = 23; 
+// if(age > 18){
+//   console.log("you can vote"); 
+// } else {
+//   console.log("you can't vote"); 
+// }
+
+// Qs 3 :- 
+
+
+
+
+// -------- 
+
+ 
+ 
+
+
 
 
 
