2d5d2274b01e422947a569d91b53f712dbf7d8f9
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-05-04 23:56:35 +0530

    Add

---
+++ Cohort3.0/Week-2/Lect-01/index.js
@@ -0,0 +1,80 @@
+
+          // functions in js 
+           
+
+
+// function sum(a, b) {
+//   return a + b; 
+// }
+
+// let ans = sum("20", 10);
+// console.log(ans); 
+
+// --------------------- 
+
+// function sum(n) {  
+//   let ans =0; 
+//   for(let i=1; i<=n; i++){ 
+//     ans = ans + i
+//   }
+//   return ans;   
+// }
+// const ans = sum(10); 
+// console.log(ans); 
+
+
+// --------------------------- 
+
+const fs = require('fs'); 
+
+const data = fs.readFileSync('b.txt', 'utf8'); 
+console.log(data);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
